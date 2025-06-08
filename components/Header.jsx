7b2c1b6b2b0c1b5ac02f445a8233ef5423d332d4
smRid <omorsk911@gmail.com>
2025-06-09 01:53:26 +0600

    Enhance pricing table and checkout page with dark theme styling; implement credit allocation logic for patients

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "./ui/button";
 import {
   Calendar,
+  CreditCard,
   ShieldCheck,
   Stethoscope,
   User,
@@ -10,11 +11,18 @@ import Link from "next/link";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { checkUser } from "@/lib/checkUser";
 import Image from "next/image";
+import { checkAndAllocateCredits } from "@/actions/credits";
+import { Badge } from "./ui/badge";
 
-const Header = async () => {
+  const Header = async () => {
   const user = await checkUser();
+  if (user?.role === "PATIENT") {
+    await checkAndAllocateCredits(user);
+  }
 
   return (
+
+    
     <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-10 supports-[backdrop-filter]:bg-background/60">
         <nav className='container mx-auto px-4 h-16 flex items-center justify-between'>
             <Link href="/">
@@ -94,6 +102,28 @@ const Header = async () => {
             )}
           </SignedIn>
 
+          {(!user || user?.role === "PATIENT") && (
+            <Link href="/pricing">
+              <Badge
+                variant="outline"
+                className="h-9 bg-emerald-900/20 border-emerald-700/30 px-3 py-1 flex items-center gap-2"
+              >
+                <CreditCard className="h-3.5 w-3.5 text-emerald-400" />
+                <span className="text-emerald-400">
+                  {user && user?.role === "PATIENT" ? (
+                    <>
+                      {user.credits}{" "}
+                      <span className="hidden md:inline">Credits</span>
+                    </>
+                  ) : (
+                    <>Pricing</>
+                  )}
+                </span>
+              </Badge>
+            </Link>
+          )}
+
+
 
         <SignedOut>
           <SignInButton>
@@ -120,4 +150,4 @@ const Header = async () => {
   )
 }
 
-export default Header
+export default Header;
